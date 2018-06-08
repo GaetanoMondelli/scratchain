d3fb5a0e94ec86417c674ada1343a8fc9d019e34
GaetanoMondelli <gaetano.mondelli@gmail.com>
2018-06-08 16:52:00 -0700

    Dynamic difficulty, removed configuration from block

--- scratchain/block.js
+++ scratchain/block.js
@@ -1,5 +1,5 @@
 const sha256 = require('js-sha256').sha256
-const { config } = require('./config')
+const { config } = require('./config') // move to blockchain (index.js)
 
 class Block{
     constructor(timestamp, back_hash_pointer, current_hash, nonce, difficulty, data){
@@ -8,7 +8,7 @@ class Block{
         this.current_hash = current_hash
         this.nonce = nonce
         this.data = data
-        this.difficulty = difficulty || config.DIFFICULTY
+        this.difficulty = difficulty
     }
 
     toString(){
@@ -17,6 +17,7 @@ class Block{
             HashPointer : ${this.back_hash_pointer.substring(0,4)}...${this.back_hash_pointer.substring(this.back_hash_pointer.length -4)}
             Nonce:      : '0x${this.nonce}'
             Difficulty  : ${this.difficulty}
+            Mine_Rate   : ${this.mine_rate}
             Hash        : ${this.current_hash.substring(0,4)}...${this.current_hash.substring(this.current_hash.length -4)}
             Data        : ${this.data}\n`;       
     }
@@ -41,10 +42,16 @@ class Block{
 
     static bruteforce_nonce(timestamp, back_hash_pointer, difficulty, data){
         let hash
+        let new_block_timestamp
         let nonce = 0
 
         while (true){
-            nonce = nonce+1
+            new_block_timestamp = Date.now()
+            nonce = nonce + 1
+            if ((new_block_timestamp - timestamp) > 3000 && difficulty > 2 )
+                {
+                    difficulty --
+                }
             hash = this.block_hash_sha256(timestamp, back_hash_pointer, nonce, data)
             if (this.containing_hash(hash, difficulty) === true){
                 return nonce
@@ -52,9 +59,9 @@ class Block{
         }
     }
 
-    static mine_block(lastBlock, data, difficulty){
-        const timestamp = Date.now()
+    static mine_block(lastBlock, data){
         const back_hash_pointer = lastBlock.current_hash
+        const { difficulty, timestamp } = lastBlock 
         const nonce = this.bruteforce_nonce(timestamp, back_hash_pointer, difficulty, data)
         const hash = this.block_hash_sha256(timestamp, back_hash_pointer, nonce, difficulty, data)
 
